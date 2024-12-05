877181d1c7280c1758edf7d33ab3bfd91af09e15
nvanzeijl <nvanzeijl@gmail.com>
2024-12-05 22:54:49 +0100

    button geregeld en landen zichtbaar nog geen styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,57 @@
+import React from 'react';
 import './App.css';
+import axios from 'axios';
+import worldmap from './assets/world_map.png';
+
 
 function App() {
 
+    const [countries, setCountries] = React.useState([]);
+    const [showButton, setShowButton] = React.useState(true);
+
+    async function fetchCountries() {
+        try {
+            const response = await axios.get('https://restcountries.com/v3.1/all');
+            console.log(response.data);
+            setCountries(response.data);
+            setShowButton(false);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+
     return (
         <>
-            Maak je applicatie hier!
+            <div className="outer-container">
+                <div className="inner-container">
+                    <img src={worldmap} alt="kaart van de wereld"/>
+                    {showButton &&
+                        <button type="button" onClick={fetchCountries}>Klik hier en laat je verrassen</button>
+                    }
+
+                    <div>
+                        {countries.map((country) => {
+                            return (
+                            <div key={country.cca3} className="country-card">
+
+                                <img src={country.flags.svg} alt={`Vlag van ${country.name.official}`}
+                                     className="country-flag"/>
+
+
+                                <h3 className="country-name">{country.name.official}</h3>
+
+
+                                <p className="country-population">
+                                    has a population of {country.population.toLocaleString()} people
+                                </p>
+                            </div>
+                            );
+                        })}
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
